75c684069a9c38f4d1390e9ffe4f0fa6700c0254
NikBel3476 <nikita.belkin3476@gmail.com>
2021-05-30 23:55:57 +0400

    Add fields login and password to Customer

--- Customer.cs
+++ Customer.cs
@@ -11,5 +11,7 @@ namespace CarShopDB
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Login { get; set; }
+        public string password { get; set; }
     }
 }
